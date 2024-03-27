8d6b98ff0cad32cca5a6dd31f9a5aee7d59c5682
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-03-26 19:54:51 -0700

    Add change grid size feature

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,7 +1,9 @@
 
 
-const gridContainer = document.querySelector(".container");
+const container = document.querySelector(".container");
 const btnSize = document.querySelector(".size");
+// let gridContainer = document.createElement("div");
+// container.appendChild(gridContainer)
 
 function createRow(size = 16){
 	let rowContainer = document.createElement('div')
@@ -18,22 +20,41 @@ function createRow(size = 16){
 }
 
 function createGrid(size = 16){
+	let gridContainer = document.createElement("div");
+	gridContainer.classList.add("gridContainer");
+
+
 	for (let i = 0; i < size; i++) {
-		gridContainer.appendChild(createRow());
+		gridContainer.appendChild(createRow(size));
 	}
+	container.appendChild(gridContainer)
 }
 
 function myScript(square) {
 	square.style.backgroundColor = 'green';
 }
 
+function getSize() {
+	let userSize
+
+	while (true) {
+		userSize = prompt("Please enter a number between 1 and 100 for the size of the grid");
+		if (Number(userSize) <= 100) {break}
+	}
+	return userSize
+}
+
+function changeSize() {
+	btnSize.addEventListener('click', () => {
+	  userSize = getSize();
+	  const gridContainer = document.querySelector(".gridContainer");
+	  container.removeChild(gridContainer);
+	  createGrid(userSize)
+	})
+}
+
 createGrid()
-
-btnSize.addEventListener('click', () => {
-  prompt("What size?")
-})
-
-
+changeSize()
 
 
 
