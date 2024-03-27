b4dfe45451534742daa19a2307439788e7cd9441
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-03-26 18:43:01 -0700

    Rename grid creation function

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -6,17 +6,27 @@ function createRow(){
 	let rowContainer = document.createElement('div')
 	for (let i = 0; i < 16; i++) {
 	  let rowSquare = document.createElement('div')
+	  rowSquare.classList.add("square");
 	  rowContainer.appendChild(rowSquare)
 	}
+	rowContainer.classList.add("row");
 	return rowContainer
 }
 
-function createRows(){
+function createGrid(){
 	for (let i = 0; i < 16; i++) {
 		gridContainer.appendChild(createRow());
 	}
 }
 
-createRow()
-createRows()
+function myScript(square) {
+	square.style.backgroundColor = 'green';
+	// console.log("lol")
+}
+
+createGrid()
+
+
+
+
 
